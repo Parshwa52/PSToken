49d25f9a958250010de5080871127a8426ef7d4f
Parshwa52 <parshwashahsbmp52@gmail.com>
2020-04-21 16:34:58 +0530

    transfer token working

--- contracts/PSToken.sol
+++ contracts/PSToken.sol
@@ -8,14 +8,15 @@ contract PSToken
     string public symbol="PS";
     string public standard="PS Token v1.0";
     mapping(address=>uint256) public balanceOf;
-    constructor(uint256 _initialSupply) public{
+    constructor(uint256 _initialSupply) public
+    {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
 
     function transfer(address _to,uint256 _value) public returns(bool)
     {
-        require(balanceOf[msg.sender] >= _value,'wont be able to transfer');
+        require(balanceOf[msg.sender] >= _value);
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         emit Transfer(msg.sender,_to,_value);
