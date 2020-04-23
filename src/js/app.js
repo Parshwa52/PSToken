0fe5c72f1c1303734ccf38e7238c73808a30c6b4
Parshwa52 <parshwashahsbmp52@gmail.com>
2020-04-23 16:32:37 +0530

    Front end sync with blockchain

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,137 @@
 
 console.log('app.js loaded');
+App={
+
+  web3Provider:null,
+  contracts:{},
+  account:'0x0',
+  loading:false,
+  tokenPrice:1000000000000000,
+  tokensSold:0,
+  tokensAvailable: 750000,
+  init: function() {
+    console.log("App initialized...")
+    return App.initWeb3();
+  },
+
+  initWeb3:function(){
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+      web3 = new Web3(App.web3Provider);
+    }
+    return App.initContracts();
+  },
+  initContracts: function() {
+    $.getJSON("PSTokenSale.json", function(psTokenSale) {
+      console.log(psTokenSale);
+      console.log(App.web3Provider);
+      console.log(App.contracts);
+      App.contracts.PSTokenSale = TruffleContract(psTokenSale);
+      App.contracts.PSTokenSale.setProvider(App.web3Provider);
+      App.contracts.PSTokenSale.deployed().then(function(psTokenSale) {
+        console.log("PS Token Sale Address:", psTokenSale.address);
+      });
+    }).done(function() {
+        $.getJSON("PSToken.json", function(psToken) {
+          App.contracts.PSToken = TruffleContract(psToken);
+          App.contracts.PSToken.setProvider(App.web3Provider);
+          App.contracts.PSToken.deployed().then(function(psToken) {
+            console.log("PSToken Address:", psToken.address);
+          });
+  
+          App.listenForEvents();
+          return App.render();
+        });
+    });
+  
+
+  },
+  render:function()
+  {
+    if (App.loading) {
+      return;
+    }
+    App.loading = true;
+
+    var loader  = $('#loader');
+    var content = $('#content');
+
+    loader.show();
+    content.hide();
+    web3.eth.getCoinbase(function(err, account) {
+      if(err === null) {
+        App.account = account;
+        $('#accountAddress').html("Your Account: " + account);
+      }
+    });
+    App.contracts.PSTokenSale.deployed().then(function(instance) {
+      psTokenSaleInstance = instance;
+      console.log(psTokenSaleInstance);
+      return psTokenSaleInstance.tokenPrice();
+    }).then(function(tokenPrice) {
+      App.tokenPrice = tokenPrice;
+      console.log(tokenPrice.toNumber());
+      $('.token-price').html(web3.fromWei(App.tokenPrice, "ether").toNumber());
+      return psTokenSaleInstance.tokensSold();
+    }).then(function(tokensSold){
+      App.tokensSold=tokensSold.toNumber();
+      $('.tokens-sold').html(App.tokensSold);
+      $('.tokens-available').html(App.tokensAvailable);
+      var progressPercent=(Math.ceil(App.tokensSold) / App.tokensAvailable) * 100;
+      console.log(progressPercent);
+      $('#progress').css('width', progressPercent + '%');
+      App.contracts.PSToken.deployed().then(function(instance) {
+        psTokenInstance = instance;
+        return psTokenInstance.balanceOf(App.account);
+      }).then(function(balance) {
+        $('.dapp-balance').html(balance.toNumber());
+        App.loading = false;
+        loader.hide();
+        content.show();
+      });
+    });
+    
+  },
+
+  buyTokens: function() {
+    $('#content').hide();
+    $('#loader').show();
+    var numberOfTokens = $('#numberOfTokens').val();
+    App.contracts.PSTokenSale.deployed().then(function(instance) {
+      return instance.buyTokens(numberOfTokens, {
+        from: App.account,
+        value: numberOfTokens * App.tokenPrice,
+        gas: 500000 // Gas limit
+      });
+    }).then(function(result) {
+      console.log("Tokens bought...")
+      $('form').trigger('reset') // reset number of tokens in form
+      // Wait for Sell event
+    });
+  },
+  listenForEvents: function() {
+    App.contracts.PSTokenSale.deployed().then(function(instance) {
+      instance.Sell({}, {
+        fromBlock: 0,
+        toBlock: 'latest',
+      }).watch(function(error, event) {
+        console.log("event triggered", event);
+        App.render();
+      })
+    })
+  }
+
+}
+$(function() {
+  $(window).on('load',function() {
+    App.init();
+  })
+});
 /*App = {
   web3Provider: null,
   contracts: {},
